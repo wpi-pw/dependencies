cf61a75c4cdd7081b04191865dc5c28532012b01
Dima Minka <bzx@mail.ru>
2019-11-21 20:33:01 +0200

    fix--bsh-multisite-url-fixer.sh--simplify require

--- bsh-multisite-url-fixer.sh
+++ bsh-multisite-url-fixer.sh
@@ -4,5 +4,4 @@
 # by DimaMinka (https://dima.mk)
 # https://github.com/wpi-pw/app
 
-composer config repositories.roots/multisite-url-fixer '{"type":"package","package": {"name": "roots/multisite-url-fixer","version": "dev-master","dist": {"url": "https://github.com/roots/multisite-url-fixer/archive/master.zip","type": "zip","reference": "master"}}}'
-composer require roots/multisite-url-fixer:dev-master --update-no-dev --quiet
+composer require roots/multisite-url-fixer --update-no-dev --quiet
