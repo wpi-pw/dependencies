27bba2a1bc4bf6c6617d5bc575a6a5517d214b84
DimaMinka <cdk@cdk.co.il>
2019-11-21 12:23:50 +0200

    feat--bsh-symlinks.sh--runner done

---
+++ bsh-symlinks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# WPI Symlinks
+# by DimaMinka (https://dima.mk)
+# https://github.com/wpi-pw/app
+
+# Get config files and put to array
+wpi_confs=()
+for ymls in wpi-config/*
+do
+  wpi_confs+=("$ymls")
+done
+
+# Get wpi-source for yml parsing, noroot, errors etc
+source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
+
+wpi_dir_symlinks cache
+wpi_dir_symlinks languages
+wpi_dir_symlinks uploads
+wpi_dir_symlinks wp-rocket-config
