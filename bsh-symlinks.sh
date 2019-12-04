067b1caac775ee23a64c7f62721ab966dc2a6e4d
Dima Minka <bzx@mail.ru>
2019-12-04 23:55:36 +0200

    chore--bsh-symlinks.sh--wpi_dir_symlinks cur env

--- bsh-symlinks.sh
+++ bsh-symlinks.sh
@@ -14,7 +14,7 @@ done
 # Get wpi-source for yml parsing, noroot, errors etc
 source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
 
-wpi_dir_symlinks cache
-wpi_dir_symlinks languages
-wpi_dir_symlinks uploads
-wpi_dir_symlinks wp-rocket-config
+wpi_dir_symlinks cache $(get_cur_env $1)
+wpi_dir_symlinks languages $(get_cur_env $1)
+wpi_dir_symlinks uploads $(get_cur_env $1)
+wpi_dir_symlinks wp-rocket-config $(get_cur_env $1)
