b3149e3c3d64b8ebeaab599c28b2c68164363e98
DimaMinka <cdk@cdk.co.il>
2019-12-17 12:10:06 +0200

    feat--mu-plugin--hello elementor override

---
+++ php-mu-hello-elementor.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Override Hello Elementor theme
+Description: https://github.com/wpi-pw/app
+Author: Dima Minka, CDK, WPI
+Version: 1.0
+Author URI: https://dima.mk
+*/
+
+// Unload default theme styles
+add_filter( 'hello_elementor_enqueue_style', false, 1 );
+add_filter( 'hello_elementor_enqueue_theme_style', false, 1 );
